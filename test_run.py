d97135fbe80a434657ca843066e23bb0f4e6e5f5
Shirley TS <49570795+ShirleyTS@users.noreply.github.com>
2025-06-19 11:52:21 +0200

    Update test_run.py

--- test_run.py
+++ test_run.py
@@ -1,11 +1,11 @@
 from google.adk.runners import Runner
-from google.adk.sessions.basic import BasicSessionService
+from google.adk.sessions.in_memory import InMemorySessionService
 from movie_finder_agent.agent import root_agent
 
 runner = Runner(
     agent=root_agent,
     app_name="movie_agent_demo",
-    session_service=BasicSessionService()
+    session_service=InMemorySessionService()
 )
 
 response = runner.run("Find a sci-fi movie from the 1990s")
